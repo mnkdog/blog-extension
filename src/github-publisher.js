000e9f2a466f63b51e864ac3deb2ba5524ad2af8
monkeydog <stephenmbrown@gmail.com>
2025-06-17 14:46:37 +0100

    Add prepareCreateRequest method for GitHub API

--- src/github-publisher.js
+++ src/github-publisher.js
@@ -25,6 +25,21 @@ ${content}`;
     
     return `${date}-${slug}.md`;
   }
+
+  prepareCreateRequest(postData) {
+    const markdown = this.createMarkdown(postData);
+    const filename = this.generateFilename(postData.title);
+    const path = `posts/${filename}`;
+    
+    // GitHub API expects base64 encoded content
+    const base64Content = btoa(markdown);
+    
+    return {
+      message: `Add new post: ${postData.title}`,
+      content: base64Content,
+      path: path
+    };
+  }
 }
 
 module.exports = GitHubPublisher;
